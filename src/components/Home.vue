6d085d3f5aa21d7f9f30bd9a5946e93b6f021d54
Hector Galvan <hectorgalvan@MacBook-Pro.local>
2023-08-04 15:39:50 -0600

    11_VariablesReactivas

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,19 @@
 <template>
-    <div>Hola</div>
+    <div>{{ obj }}</div>
 </template>
 <script>
-import { onMounted } from "vue";
+// import { ref } from "vue"; varibales
+import { reactive } from "vue"; // objetos
 export default {
     setup() {
-        onMounted(() => {
-            console.log("Munted");
-        });
+        // const counter = ref(0);
+        // setInterval(() => counter.value++, 500);
+        const obj = reactive({ counter: 0 });
+        setInterval(() => obj.counter++, 500);
+
+        return {
+            obj,
+        };
     },
 };
 </script>
