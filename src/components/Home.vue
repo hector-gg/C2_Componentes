dcb830856cb68167914184cb6ad6cd61bcea2e52
Hector Galvan <hectorgalvan@MacBook-Pro.local>
2023-08-07 15:40:54 -0600

    11_watch

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,24 @@
 <template>
     <div>{{ obj }}</div>
+    <!-- <div>{{ counter }}</div> -->
 </template>
 <script>
-// import { ref } from "vue"; varibales
-import { reactive } from "vue"; // objetos
+import { ref } from "vue"; //Valores
+import { reactive, watch } from "vue"; // objetos
 export default {
     setup() {
-        // const counter = ref(0);
-        // setInterval(() => counter.value++, 500);
+        // const counter = ref(0); //ref
+        // setInterval(() => counter.value++, 500); //ref
         const obj = reactive({ counter: 0 });
         setInterval(() => obj.counter++, 500);
+        watch(
+            () => obj.counter,
+            (valor, anterior) => {
+                //reactive
+                // watch(counter, (valor, anterior) => { //ref
+                console.log(valor, anterior);
+            }
+        );
 
         return {
             obj,
